d588dc5a80d278cae78b69031243b83fe6514121
rendy-darma-rd <darmarendy@gmail.com>
2026-05-18 22:33:49 +0700

    Fix all dependency versions and add Cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,9 +5,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://example.com',
+  adapter: cloudflare(),
   integrations: [mdx(), sitemap(), react(), markdoc(), keystatic()],
   output: 'static',
 });
