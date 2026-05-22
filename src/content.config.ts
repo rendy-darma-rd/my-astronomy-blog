11ccb1497d76d0342c4acb9bc20e0232365df0b2
rendy-darma-rd <darmarendy@gmail.com>
2026-05-22 17:07:45 +0800

    Improve thumbnail image size and bind the articles to the cards on homepage

--- src/content.config.ts
+++ src/content.config.ts
@@ -3,8 +3,8 @@ import { glob } from 'astro/loaders';
 import { z } from 'astro/zod';
 
 const blog = defineCollection({
-	// Load Markdown and MDX files in the `src/content/blog/` directory.
-	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx}' }),
+	// Load Markdown, MDX, and MDOC files in the `src/content/blog/` directory.
+	loader: glob({ base: './src/content/blog', pattern: '**/*.{md,mdx,mdoc}' }),
 	// Type-check frontmatter using a schema
 	schema: ({ image }) =>
 		z.object({
@@ -13,7 +13,7 @@ const blog = defineCollection({
 			// Transform string to Date object
 			pubDate: z.coerce.date(),
 			updatedDate: z.coerce.date().optional(),
-			heroImage: z.optional(image()),
+			heroImage: z.union([z.string(), image()]).optional(),
 		}),
 });
 
