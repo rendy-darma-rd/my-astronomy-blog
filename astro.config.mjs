340b097948c959ffe3005b550de99d7c603e292c
rendy-darma-rd <darmarendy@gmail.com>
2026-05-25 13:40:33 +0800

    Disable route /keystatic on production

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,15 @@ import markdoc from '@astrojs/markdoc';
 import keystatic from '@keystatic/astro';
 import cloudflare from '@astrojs/cloudflare';
 
+// Only include Keystatic in development
+const integrations = [mdx(), sitemap(), react(), markdoc()];
+if (process.env.NODE_ENV !== 'production') {
+  integrations.push(keystatic());
+}
+
 export default defineConfig({
   site: 'https://example.com',
   adapter: cloudflare(),
-  integrations: [mdx(), sitemap(), react(), markdoc(), keystatic()],
+  integrations,
   output: 'static',
 });
