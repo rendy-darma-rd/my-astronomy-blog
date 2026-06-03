10eae487b8b2f1a82517b49e11660f95e2e8ab42
rendy-darma-rd <darmarendy@gmail.com>
2026-06-03 15:39:23 +0800

    Finishing page Tentang Kami

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Antariksa';
+export const SITE_DESCRIPTION = 'Menyajikan beragam artikel astronomi yang menarik dan mudah dipahami untuk semua kalangan.';
